b3314c3a559d21386794c138f365ce46b248916b
Peter Kreppein <pmkreppein@gmail.com>
2018-09-17 20:27:59 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -8,3 +8,11 @@ end
 puts "HAPPY NEW YEAR!"
 end
 
+def countdown_with_sleep(number)
+  while number >= 0
+    puts "#{number} SECOND(S)!"
+    number -= 1
+    sleep(1)
+end
+puts "HAPPY NEW YEAR!"
+end
